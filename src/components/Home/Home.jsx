f67f3fd014cc062cad7e4a34e7389ed41c14725c
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-11-17 00:14:32 +0600

    image added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Home.css';
+import image from '../../images/Recharge-your-down-battery.jpg';
 
 const Home = () => {
     return (
-        <div>
+        <div className='book-container'>
             <div className='details'>
                 <h1>Recharge Your <br />
                     <span className='own-battery'>Own Battery</span>
                 </h1>
                 <p>Those who are ahead in tune with the times, various initiatives are being taken to take them one step further. But those who are a little behind, even from a good position, who have lost for some reason, will they be disappointed?</p>
             </div>
-            <div>
-                <img src="" alt="" />
+            <div className='cover-image'>
+                <img src={image} alt="" />
             </div>
         </div>
     );
