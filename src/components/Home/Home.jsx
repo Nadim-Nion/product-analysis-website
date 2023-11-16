db2ec9a5283101c3ccffbdce1f9076b51c3c7963
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-11-17 00:06:54 +0600

    header and route added

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <div className='details'>
+                <h1>Recharge Your <br />
+                    <span className='own-battery'>Own Battery</span>
+                </h1>
+                <p>Those who are ahead in tune with the times, various initiatives are being taken to take them one step further. But those who are a little behind, even from a good position, who have lost for some reason, will they be disappointed?</p>
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/Layout/Main.jsx';
+import Home from './components/Home/Home.jsx';
+import Reviews from './components/Reviews/Reviews.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import Blogs from './components/Blogs/Blogs.jsx';
+import About from './components/About/About.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'reviews',
+        element: <Reviews></Reviews>
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard></Dashboard>
+      },
+      {
+        path: 'blogs',
+        element: <Blogs></Blogs>
+      },
+      {
+        path: 'about',
+        element: <About></About>
+      }
+    ],
+
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
